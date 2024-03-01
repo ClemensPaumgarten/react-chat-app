7dd374af8e4f46531e803e779e2ac022a1eaf249
Clemens Paumgarten <mail@clemens-paumgarten.com>
2024-03-01 09:40:26 +0100

    finish up user slice

--- src/slice/userSlice.ts
+++ src/slice/userSlice.ts
@@ -2,6 +2,16 @@ import { User } from "../models/user.ts";
 import { createSlice } from "@reduxjs/toolkit";
 import { useAppSelector } from "../store/store.ts";
 
+const getUserFromLocalStorage = () => {
+  const user = localStorage.getItem("user");
+
+  if (user) {
+    return JSON.parse(user) as User;
+  }
+
+  return null;
+};
+
 export type UserStore = {
   user: User | null;
 };
@@ -9,7 +19,7 @@ export type UserStore = {
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    user: null,
+    user: getUserFromLocalStorage(),
   } as UserStore,
   reducers: {
     setUser: (state, action) => {
