cf9d779cb22166df8d5b3da58f0440df47421267
Clemens Paumgarten <mail@clemens-paumgarten.com>
2024-03-27 12:26:28 +0100

    use chat container

--- src/components/Chat/ChatSelection.tsx
+++ src/components/Chat/ChatSelection.tsx
@@ -1,10 +1,9 @@
 import { FunctionComponent } from "react";
 import { useChatStore } from "../../store/chatStore.tsx";
-import { Chat } from "./Chat.tsx";
 import { SelectChat } from "../SelectChat/SelectChat.tsx";
 
 export const ChatContainer: FunctionComponent = () => {
   const { currentChatRoom } = useChatStore();
 
-  return currentChatRoom ? <Chat /> : <SelectChat />;
+  return currentChatRoom ? <ChatContainer /> : <SelectChat />;
 };
